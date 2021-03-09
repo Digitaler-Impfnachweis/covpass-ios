9334950b420960734782297cececca6fe397014e
Daniel Mandea <daniel.mandea@ro.ibm.com>
2021-03-09 15:37:51 +0200

    Initial commit.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Daniel on 09.03.2021.
+//
+
+import SwiftUI
+import VaccinationPass
+import VaccinationValidator
+import VaccinationCommon
+
+struct ContentView: View {
+    var body: some View {
+        DashboardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/VaccinationCertificateApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccinationCertificateApp.swift
+//  Shared
+//
+//  Created by Daniel on 09.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct VaccinationCertificateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
