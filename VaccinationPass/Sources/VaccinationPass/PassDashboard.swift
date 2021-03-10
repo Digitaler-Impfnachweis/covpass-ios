7ddc5cf0e95f8a1d76b7c9082b5a581a0981ee83
Daniel Mandea <daniel.mandea@ro.ibm.com>
2021-03-10 17:25:59 +0200

    Added ios 13 setup

---
+++ VaccinationPass/Sources/VaccinationPass/PassDashboard.swift
@@ -0,0 +1,24 @@
+//
+//  PassDashboard.swift
+//  
+//
+//  Created by Daniel on 10.03.2021.
+//
+
+import SwiftUI
+import VaccinationCommon
+
+public struct PassDashboard: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        DashboardView()
+    }
+}
+
+struct PassDashboard_Previews: PreviewProvider {
+    static var previews: some View {
+        PassDashboard()
+    }
+}
--- VaccinationValidator/Package.swift
+++ VaccinationValidator/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "VaccinationValidator",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VaccinationValidator",
-            dependencies: []),
+            dependencies: ["VaccinationCommon"]),
         .testTarget(
             name: "VaccinationValidatorTests",
             dependencies: ["VaccinationValidator"]),
