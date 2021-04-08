980b95dbc2e946a17ae2f1944a08d7c6f390cf57
Gabriela Secelean <gabriela.secelean@ibm.com>
2021-04-08 16:20:22 +0300

    Implement a second decompression algorithm

--- VaccinationCommon/Sources/VaccinationCommon/Processing/QRCodeProcessor.swift
+++ VaccinationCommon/Sources/VaccinationCommon/Processing/QRCodeProcessor.swift
@@ -5,6 +5,7 @@
 //  Copyright © 2021 IBM. All rights reserved.
 //
 
+import Compression
 import Foundation
 import SwiftCBOR
 
@@ -18,12 +19,24 @@ public class QRCodeProcessor {
         print("Base45 Decoded: \(base45Decoded)")
         let base45Encoded = base45Encoder.encode(base45Decoded)
         print("Base45 works fine: \(base45Encoded == payload)")
-        let decompressedPayload = Data(base45Decoded).decompress(withAlgorithm: .zlib) ?? Data()
+        let decompressedPayload = decompress(Data(base45Decoded)) ?? Data()
         print("Decompressed payload: \(decompressedPayload)")
         let cosePayload = CodeSign1Encoder.parse(decompressedPayload)
         print("COSE batshit: \(String(describing: cosePayload))")
         let cborDecodedPayload = CBOR.decode(cosePayload?.payload ?? [])
 
         print("God help us with: \(String(describing: cborDecodedPayload))")
+    }
+
+    func decompress(_ data: Data) -> Data? {
+        let size = 8_000_000
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
+        let result = data.subdata(in: 2 ..< data.count).withUnsafeBytes ({
+            let read = compression_decode_buffer(buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1),
+                                                 data.count - 2, nil, COMPRESSION_ZLIB)
+            return Data(bytes: buffer, count:read)
+        }) as Data
+        buffer.deallocate()
+        return result
     }
 }
