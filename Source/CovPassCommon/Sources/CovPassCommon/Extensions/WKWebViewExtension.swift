b4a73b20f88d2c0cb830f545dda04860f2cd6eb7
Timo Koenig <timo@timokoenig.com>
2021-05-21 10:28:09 +0200

    Add text scaling for web view content

---
+++ Source/CovPassCommon/Sources/CovPassCommon/Extensions/WKWebViewExtension.swift
@@ -0,0 +1,29 @@
+//
+//  StringExtension.swift
+//
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import WebKit
+
+public extension WKWebView {
+    func enableTextScaling() {
+        let font = UIFont.systemFont(ofSize: 10.0)
+        let fontMetrics = UIFontMetrics(forTextStyle: .body)
+        let scaledFont = fontMetrics.scaledFont(for: font, compatibleWith: traitCollection)
+        let scalingFactor = Int(((scaledFont.pointSize / font.pointSize) * 100) - 100)
+        let cssStyle = """
+            javascript:(function() {
+            var parent = document.getElementsByTagName('head').item(0);
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = '* { font-size: \(scalingFactor)%; }';
+            parent.appendChild(style)})()
+        """
+        let script = WKUserScript(source: cssStyle, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        self.configuration.userContentController.addUserScript(script)
+    }
+}
