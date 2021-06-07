f642ff30f36585a65ccaa485439b7d666b147c24
Timo Koenig <timo@timokoenig.com>
2021-06-07 16:07:08 +0200

    Add app icon to snapshot background util

--- Source/CovPassUI/Sources/CovPassUI/Utils/BackgroundUtils.swift
+++ Source/CovPassUI/Sources/CovPassUI/Utils/BackgroundUtils.swift
@@ -10,17 +10,32 @@ import UIKit
 
 public class BackgroundUtils {
     static let hideViewTag = 01_071_991
-    static let imageViewHeight: CGFloat = 300
+    static let imageViewSize: CGFloat = 100
+
+    static var appIcon: UIImage? {
+        if let icons = Bundle.main.infoDictionary?["CFBundleIcons"] as? [String: Any],
+           let primary = icons["CFBundlePrimaryIcon"] as? [String: Any],
+           let files = primary["CFBundleIconFiles"] as? [String],
+           let icon = files.last {
+          return UIImage(named: icon)
+        }
+        return nil
+    }
 
     public static func addHideView(to window: UIWindow?) {
         guard let window = window else { return }
         let hideView = UIView(frame: window.frame)
         hideView.backgroundColor = .white
-        let imageView = UIImageView(frame: .init(x: 0, y: 0, width: hideView.frame.width, height: BackgroundUtils.imageViewHeight))
-        imageView.image = .onboardingScreen1
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = appIcon
         hideView.addSubview(imageView)
         hideView.tag = BackgroundUtils.hideViewTag
         window.addSubview(hideView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: hideView.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: hideView.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: imageViewSize).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: imageViewSize).isActive = true
     }
 
     public static func removeHideView(from window: UIWindow?) {
