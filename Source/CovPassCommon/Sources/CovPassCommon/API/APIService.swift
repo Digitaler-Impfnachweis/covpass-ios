72f6fc1fbe64566a3a4041e0db432643d734999a
Timo Koenig <timo@timokoenig.com>
2021-06-09 23:55:29 +0200

    Remove unused property

--- Source/CovPassCommon/Sources/CovPassCommon/API/APIService.swift
+++ Source/CovPassCommon/Sources/CovPassCommon/API/APIService.swift
@@ -13,8 +13,6 @@ import SwiftCBOR
 public struct APIService: APIServiceProtocol {
     private let url: String
     private let contentType: String = "application/cbor+base45"
-
-    private let coder = Base45Coder()
     private let sessionDelegate: URLSessionDelegate
 
     public init(sessionDelegate: URLSessionDelegate, url: String) {
