8ee32c809f1e3ac35d053fbfdf109ce331896a7d
Timo Koenig <timo@timokoenig.com>
2021-07-25 22:01:14 +0200

    Add unit tests

--- Source/CovPassCommon/Tests/CovPassCommonTests/Utils/CheckAppUpdateTests.swift
+++ Source/CovPassCommon/Tests/CovPassCommonTests/Utils/CheckAppUpdateTests.swift
@@ -12,7 +12,7 @@ import PromiseKit
 
 @testable import CovPassCommon
 
-struct CheckAppUpdateServiceMock: CheckAppUpdateServiceProtocol {
+class CheckAppUpdateServiceMock: CheckAppUpdateServiceProtocol {
     var versionResult: Promise<String>?
     func getAppStoreVersion() -> Promise<String> {
         return versionResult ?? Promise.value("")
@@ -22,7 +22,7 @@ struct CheckAppUpdateServiceMock: CheckAppUpdateServiceProtocol {
 class CheckAppUpdateTests: XCTestCase {
 
     func testAppUpdate() throws {
-        var service = CheckAppUpdateServiceMock()
+        let service = CheckAppUpdateServiceMock()
         service.versionResult = Promise.value("1.0.1")
         let userDefaults = MockPersistence()
         var sut = CheckAppUpdate(service: service, userDefaults: userDefaults, appStoreID: "1")
@@ -34,7 +34,7 @@ class CheckAppUpdateTests: XCTestCase {
     }
 
     func testAppUpdateNothingNew() throws {
-        var service = CheckAppUpdateServiceMock()
+        let service = CheckAppUpdateServiceMock()
         service.versionResult = Promise.value("1.0.0")
         let userDefaults = MockPersistence()
         var sut = CheckAppUpdate(service: service, userDefaults: userDefaults, appStoreID: "1")
@@ -46,7 +46,7 @@ class CheckAppUpdateTests: XCTestCase {
 
     func testAppUpdateAlreadyConfirmed() {
         do {
-            var service = CheckAppUpdateServiceMock()
+            let service = CheckAppUpdateServiceMock()
             service.versionResult = Promise.value("1.0.1")
             let userDefaults = MockPersistence()
             try userDefaults.store(UserDefaults.keyCheckVersionUpdate, value: "1.0.0")
@@ -58,5 +58,70 @@ class CheckAppUpdateTests: XCTestCase {
         } catch {
             XCTAssert(type(of: error) == PromiseCancelledError.self)
         }
+    }
+
+    func testAppUpdateFullFlow() throws {
+        // New Update avilable
+        let service = CheckAppUpdateServiceMock()
+        service.versionResult = Promise.value("1.0.1")
+        let userDefaults = MockPersistence()
+        var sut = CheckAppUpdate(service: service, userDefaults: userDefaults, appStoreID: "1")
+        sut.bundleVersion = "1.0.0"
+
+        var appUpdate = try sut.updateAvailable().wait()
+        XCTAssert(appUpdate.shouldUpdate)
+        XCTAssertEqual(appUpdate.version, "1.0.1")
+
+        // Update App
+        sut.bundleVersion = "1.0.1"
+
+        // Everything is up to date
+        appUpdate = try sut.updateAvailable().wait()
+        XCTAssertFalse(appUpdate.shouldUpdate)
+
+        // New app version in store
+        service.versionResult = Promise.value("1.1")
+
+        // App update is available
+        appUpdate = try sut.updateAvailable().wait()
+        XCTAssert(appUpdate.shouldUpdate)
+        XCTAssertEqual(appUpdate.version, "1.1")
+
+        // User declines dialog for 1.1
+        try userDefaults.store(UserDefaults.keyCheckVersionUpdate, value: "1.0.1")
+
+        // No dialog because user declined previous one
+        do {
+            _ = try sut.updateAvailable().wait()
+            XCTFail("Should fail")
+        } catch {
+            XCTAssert(type(of: error) == PromiseCancelledError.self)
+        }
+
+        // New app version in store
+        service.versionResult = Promise.value("1.2")
+
+        // Still no dialog because user declined previous one
+        do {
+            _ = try sut.updateAvailable().wait()
+            XCTFail("Should fail")
+        } catch {
+            XCTAssert(type(of: error) == PromiseCancelledError.self)
+        }
+
+        // Update App
+        sut.bundleVersion = "1.2"
+
+        // Everything is up to date
+        appUpdate = try sut.updateAvailable().wait()
+        XCTAssertFalse(appUpdate.shouldUpdate)
+
+        // New app version in store
+        service.versionResult = Promise.value("1.3")
+
+        // Now a new app update is available and the dialog is displayed
+        appUpdate = try sut.updateAvailable().wait()
+        XCTAssert(appUpdate.shouldUpdate)
+        XCTAssertEqual(appUpdate.version, "1.3")
     }
 }
