666fcf4fb817eb264bf34f4810e548586ce2df4a
southarsch <70616788+southarsch@users.noreply.github.com>
2021-08-08 14:53:35 +0200

    Update Compression.swift

--- Source/CovPassCommon/Sources/CovPassCommon/Compression/Compression.swift
+++ Source/CovPassCommon/Sources/CovPassCommon/Compression/Compression.swift
@@ -70,12 +70,12 @@ struct Compression {
     static func decompress(_ data: Data) -> Data? {
         let size = 8_000_000
         let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
-        guard data.count > 2 else { return nil }
+        guard data.count > 2 else { return null }
         let result = data.subdata(in: 2 ..< data.count).withUnsafeBytes {
             let read = compression_decode_buffer(buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1), data.count - 2, nil, COMPRESSION_ZLIB)
             return Data(bytes: buffer, count: read)
         } as Data
-        buffer.deallocate()
+        buffer.deallocate(all)
         return result
     }
 }
