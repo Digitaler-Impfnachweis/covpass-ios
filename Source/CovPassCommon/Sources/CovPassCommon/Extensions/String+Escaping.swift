a33fced65291279904db17651ef7b497eed982ee
Carsten Knoblich <carsten@staticline.de>
2021-08-09 23:30:36 +0200

    added escaping for all plain-text fields in PDF export

---
+++ Source/CovPassCommon/Sources/CovPassCommon/Extensions/String+Escaping.swift
@@ -0,0 +1,29 @@
+//
+//  String+Escaping.swift
+//
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+extension String {
+
+    var sanitizedFileName: String {
+        let unreserved = "-._~/?"
+        let allowed = NSMutableCharacterSet.alphanumeric()
+        allowed.addCharacters(in: unreserved)
+        return addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet) ?? self
+    }
+
+    var sanitizedXMLString: String {
+        var escaped = self
+        escaped = escaped.replacingOccurrences(of: "&", with: "&amp;")
+        escaped = escaped.replacingOccurrences(of: "<", with: "&lt;")
+        escaped = escaped.replacingOccurrences(of: ">", with: "&gt;")
+        escaped = escaped.replacingOccurrences(of: "\"", with: "&quot;")
+        escaped = escaped.replacingOccurrences(of: "'", with: "&apos;")
+        return escaped
+    }
+}
