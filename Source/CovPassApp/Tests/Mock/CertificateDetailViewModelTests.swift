d85f5b57a0f73f078ceaf8ca8f3cb2eae88e3428
Carsten Knoblich-external <carsten@staticline.de>
2021-08-20 11:06:28 +0200

    Fix/labels (#25)
    * fixed name order
    
    * adjusted information text if fully vaccinated but within the first 14 days timespan
    
    * added stub for tests
    
    * fixed different name formats based on label type
    
    * using selected certificate instead of full list

---
+++ Source/CovPassApp/Tests/Mock/CertificateDetailViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  CertificateDetailViewModelTests.swift
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+
+class CertificateDetailViewModelTests: XCTestCase {
+
+    func testImmunizationTitle() throws {
+        throw XCTSkip("currently not implemented")
+    }
+
+    func testImmunizationBody() throws {
+        throw XCTSkip("currently not implemented")
+    }
+
+}
