e75027894d1b05005f10e219848e198edcf37373
Carsten Knoblich <carsten@staticline.de>
2021-08-29 11:32:06 +0200

    extended existing hint view to show optional subtitle

--- Source/CovPassUI/Sources/CovPassUI/UIComponents/HintView.swift
+++ Source/CovPassUI/Sources/CovPassUI/UIComponents/HintView.swift
@@ -13,8 +13,11 @@ public class HintView: XibView {
 
     @IBOutlet public var iconView: UIImageView!
     @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var subTitleLabel: UILabel!
     @IBOutlet public var bodyLabel: UILabel!
     @IBOutlet public var containerView: UIView!
+
+    @IBOutlet public var subTitleConstraint: NSLayoutConstraint!
 
     // MARK: - Lifecycle
 
@@ -27,5 +30,16 @@ public class HintView: XibView {
         containerView?.layer.cornerRadius = 12.0
 
         iconView.image = UIImage.warning
+
+        // remove placeholders
+        titleLabel.text = nil
+        subTitleLabel.text = nil
+        bodyLabel.text = nil
+    }
+
+    public override func updateConstraints() {
+        // no subtitle? disable this constraint and align to `titleLabel`
+        subTitleConstraint.isActive = !(subTitleLabel.text?.isEmpty ?? true)
+        super.updateConstraints()
     }
 }
