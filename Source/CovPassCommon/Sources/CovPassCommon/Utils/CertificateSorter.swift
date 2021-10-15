75f4d3e00f2cc14cb1ca55d7826bb071dcdb1e40
Fatih Karakurt-external <hallo@fatihkarakurt.de>
2021-10-15 15:09:08 +0200

    BVC-2341:  Sorting Certificates (#97)
    * possible fix where for sorting also iat is used. also add some tests for that
    
    * change test data
    
    * refactor and add issued at time logic
    
    * remove unnecessary method

--- Source/CovPassCommon/Sources/CovPassCommon/Utils/CertificateSorter.swift
+++ Source/CovPassCommon/Sources/CovPassCommon/Utils/CertificateSorter.swift
@@ -8,105 +8,179 @@
 
 import Foundation
 
+extension ExtendedCBORWebToken {
+    
+    var tests: [Test]? { vaccinationCertificate.hcert.dgc.t }
+
+    var recoveries: [Recovery]? { vaccinationCertificate.hcert.dgc.r }
+
+    var vaccinations: [Vaccination]? { vaccinationCertificate.hcert.dgc.v }
+
+    var firstTest: Test? { tests?.first }
+    
+    var firstRecovery: Recovery? { recoveries?.first }
+    
+    var firstVaccination: Vaccination? { vaccinations?.first }
+    
+}
+
+extension Array where Element == ExtendedCBORWebToken {
+    
+    var sortByIssuedAtTime: [ExtendedCBORWebToken] {
+        sorted(by: { c1, c2 -> Bool in
+            print("TEST")
+            guard let c1Iat = c1.vaccinationCertificate.iat else {
+                return false
+            }
+            guard let c2Iat = c2.vaccinationCertificate.iat else {
+                return true
+            }
+            return c1Iat >= c2Iat
+        })
+    }
+    
+    var sortTestsByDateTimeOfSampleCollection: [ExtendedCBORWebToken] {
+        sorted(by: {
+            guard let lhs = $0.firstTest?.sc,
+                    let rhs = $1.firstTest?.sc else {
+                return false
+            }
+            return lhs > rhs
+        })
+    }
+    
+    var sortByVaccinationDate: [ExtendedCBORWebToken] {
+        sorted(by: { c1, c2 -> Bool in
+            return c1.firstVaccination?.dt ?? Date() > c2.firstVaccination?.dt ?? Date()
+        })
+    }
+    
+    var filterNegativePCRTestsNotOlderThan72Hours: [ExtendedCBORWebToken] {
+        filter {
+            if let t = $0.firstTest, t.isPCR, !t.isPositive, Date() <= Calendar.current.date(byAdding: .hour, value: 72, to: t.sc) ?? Date() {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterNegativeQuickTestsNotOlderThan48Hours: [ExtendedCBORWebToken] {
+        filter {
+            if let t = $0.firstTest, !t.isPCR, !t.isPositive, Date() <= Calendar.current.date(byAdding: .hour, value: 48, to: t.sc) ?? Date() {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterBoosted: [ExtendedCBORWebToken] {
+        filter {
+            if let v = $0.firstVaccination, v.isBoosted {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterNotFullImmunization: [ExtendedCBORWebToken] {
+        filter {
+            if let v = $0.firstVaccination, !v.fullImmunization {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterVaccinations: [ExtendedCBORWebToken] {
+        filter { $0.vaccinations != nil }
+    }
+    
+    var firstNotBosstedValidFullImmunization: ExtendedCBORWebToken? {
+        first(where: {
+            if let v = $0.firstVaccination, v.fullImmunization, v.fullImmunizationValid, !v.isBoosted /* Boosters are currently lower prioritized (see 5.1) */ {
+                return true
+            }
+            return false
+        })
+    }
+    
+    var firstNotValidButFullImmunization: ExtendedCBORWebToken? {
+        first(where: {
+            if let v = $0.firstVaccination, v.fullImmunization, !v.fullImmunizationValid {
+                return true
+            }
+            return false
+        })
+    }
+    
+    var filterRecoveryWhoseDateIsStillValid: [ExtendedCBORWebToken]  {
+        filter {
+            if let r = $0.firstRecovery, Date() <= r.du {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterRecoveryWhoseDateNotAnyMoreValid: [ExtendedCBORWebToken]  {
+        filter {
+            if let r = $0.firstRecovery, Date() > r.du {
+                return true
+            }
+            return false
+        }
+    }
+    
+    var filterAllTestsNegativeAndNotValid: [ExtendedCBORWebToken] {
+        filter {
+            if let t = $0.firstTest, !t.isPositive, !t.isValid {
+                return true
+            }
+            return false
+        }
+    }
+}
+
 public enum CertificateSorter {
     /// Sorts the certificates by given order
     public static func sort(_ certificates: [ExtendedCBORWebToken]) -> [ExtendedCBORWebToken] {
         var res = [ExtendedCBORWebToken]()
         // #1 Test certificate
         //  Negative PCR Test not older than (=<)72h, ordered by date (newest first)
-        let pcrTests = certificates.filter {
-            if let t = $0.vaccinationCertificate.hcert.dgc.t?.first, t.isPCR, !t.isPositive, Date() <= Calendar.current.date(byAdding: .hour, value: 72, to: t.sc) ?? Date() {
-                return true
-            }
-            return false
-        }
-        res.append(contentsOf: pcrTests.sorted(by: {
-            guard
-                let lhs = $0.vaccinationCertificate.hcert.dgc.t?.first?.sc,
-                let rhs = $1.vaccinationCertificate.hcert.dgc.t?.first?.sc
-            else {
-                return false
-            }
-            return lhs > rhs
-        }))
+        let pcrTests = certificates.filterNegativePCRTestsNotOlderThan72Hours
+        res.append(contentsOf: pcrTests.sortByIssuedAtTime.sortTestsByDateTimeOfSampleCollection)
         // #2 Test certificate
         //  Negative quick test, not older than 48 hrs, ordered by date (newest first)
-        let quickTests = certificates.filter {
-            if let t = $0.vaccinationCertificate.hcert.dgc.t?.first, !t.isPCR, !t.isPositive, Date() <= Calendar.current.date(byAdding: .hour, value: 48, to: t.sc) ?? Date() {
-                return true
-            }
-            return false
-        }
-        res.append(contentsOf: quickTests.sorted(by: {
-            guard
-                let lhs = $0.vaccinationCertificate.hcert.dgc.t?.first?.sc,
-                let rhs = $1.vaccinationCertificate.hcert.dgc.t?.first?.sc
-            else {
-                return false
-            }
-            return lhs > rhs
-        }))
+        var quickTests = certificates.filterNegativeQuickTestsNotOlderThan48Hours
+        quickTests  = quickTests.sortByIssuedAtTime
+        quickTests  = quickTests.sortTestsByDateTimeOfSampleCollection
+        res.append(contentsOf: quickTests)
         // #3 Booster Certificate
         //  Latest booster vaccination of a vaccination series (3/3, 4/4, ...)
-        res.append(contentsOf: certificates.filter {
-            if let v = $0.vaccinationCertificate.hcert.dgc.v?.first, v.isBoosted {
-                return true
-            }
-            return false
-        })
+        res.append(contentsOf: certificates.filterBoosted)
         // #4 Vaccination certificate
-        //  Latest vaccination of a vaccination series (1/1, 2/2), older then (>) 14 days
-        let vaccinationCertificates = certificates.filter { $0.vaccinationCertificate.hcert.dgc.v != nil }.sorted(by: { c1, c2 -> Bool in c1.vaccinationCertificate.hcert.dgc.v?.first?.dt ?? Date() > c2.vaccinationCertificate.hcert.dgc.v?.first?.dt ?? Date() })
-        if let latestVaccination = vaccinationCertificates.first(where: {
-            if let v = $0.vaccinationCertificate.hcert.dgc.v?.first, v.fullImmunization, v.fullImmunizationValid, !v.isBoosted /* Boosters are currently lower prioritized (see 5.1) */ {
-                return true
-            }
-            return false
-        }) {
+        //  Latest vaccination of a vaccination series (1/1, 2/2), older then (>) 14 days, and where the iat is the latest
+        let vaccinationCertificates = certificates.filterVaccinations.sortByIssuedAtTime.sortByVaccinationDate
+        if let latestVaccination = vaccinationCertificates.firstNotBosstedValidFullImmunization {
             res.append(latestVaccination)
         }
         // #5 Recovery certificate
         //  Recovery after SARS-Cov-2-Infection, not older then (=<) 180 Days
-        res.append(contentsOf: certificates.filter {
-            if let r = $0.vaccinationCertificate.hcert.dgc.r?.first, Date() <= r.du {
-                return true
-            }
-            return false
-        })
+        res.append(contentsOf: certificates.filterRecoveryWhoseDateIsStillValid)
         // #6 Vaccination Certificate
         //  Latest vaccination of a vaccination series, not older then (=<) 14 days
-        if let latestVaccination = vaccinationCertificates.first(where: {
-            if let v = $0.vaccinationCertificate.hcert.dgc.v?.first, v.fullImmunization, !v.fullImmunizationValid {
-                return true
-            }
-            return false
-        }) {
+        if let latestVaccination = vaccinationCertificates.firstNotValidButFullImmunization {
             res.append(latestVaccination)
         }
         // #7 Vaccination Certificate
         //  Not-latest (partial immunization) of a vaccination series (1/2)
-        res.append(contentsOf: certificates.filter {
-            if let v = $0.vaccinationCertificate.hcert.dgc.v?.first, !v.fullImmunization {
-                return true
-            }
-            return false
-        })
+        res.append(contentsOf: certificates.filterNotFullImmunization)
         // #8 Recovery Certificate
         //  Recovery after SARS-Cov-2-Infection, older then (>) 180 Days
-        res.append(contentsOf: certificates.filter {
-            if let r = $0.vaccinationCertificate.hcert.dgc.r?.first, Date() > r.du {
-                return true
-            }
-            return false
-        })
+        res.append(contentsOf: certificates.filterRecoveryWhoseDateNotAnyMoreValid)
         // #9 Test certificate
         //  Negative PCR-Test, older then (>) 72 Hrs, or negative quick test older then (>) 48 Hrs
-        res.append(contentsOf: certificates.filter {
-            if let t = $0.vaccinationCertificate.hcert.dgc.t?.first, !t.isPositive, !t.isValid {
-                return true
-            }
-            return false
-        })
+        res.append(contentsOf: certificates.filterAllTestsNegativeAndNotValid)
         // #10 Now add everything that did not match any of the rules above
         res.append(contentsOf: certificates.filter { !res.contains($0) })
 
--- Source/CovPassCommon/Tests/CovPassCommonTests/Utils/CertificateSorterTests.swift
+++ Source/CovPassCommon/Tests/CovPassCommonTests/Utils/CertificateSorterTests.swift
@@ -18,8 +18,14 @@ extension CBORWebToken {
         token.hcert.dgc.nam = name
         return token
     }
+    
     func mockVaccinationUVCI(_ uvci: String) -> Self {
         hcert.dgc.v?.first?.ci = uvci
+        return self
+    }
+    
+    func mockTestUVCI(_ uvci: String) -> Self {
+        hcert.dgc.t?.first?.ci = uvci
         return self
     }
 
@@ -28,12 +34,14 @@ extension CBORWebToken {
         return self
     }
 
-    func extended() -> ExtendedCBORWebToken {
-        ExtendedCBORWebToken(vaccinationCertificate: self, vaccinationQRCodeData: "")
+    func extended(vaccinationQRCodeData: String = "") -> ExtendedCBORWebToken {
+        ExtendedCBORWebToken(vaccinationCertificate: self,
+                             vaccinationQRCodeData: vaccinationQRCodeData)
     }
 }
 
 class CertificateSorterTests: XCTestCase {
+    
     func testSorting() throws {
         let certificates = [
             CBORWebToken
@@ -52,5 +60,399 @@ class CertificateSorterTests: XCTestCase {
         XCTAssertEqual(sortedCertifiates.count, 2)
         XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "2")
         XCTAssertEqual(sortedCertifiates[1].vaccinationCertificate.hcert.dgc.uvci, "1")
+    }
+    
+    func testLatestIatCertOnSameVaccinationDate() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-26T15:05:00")
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-27T15:05:00")
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-28T15:05:00")
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            thirdCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 3)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "3")
+    }
+    
+    func testLatestIatCertOnSameVaccinationDateButOneOfThemHasNoIat() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-26T15:05:00")
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-27T15:05:00")
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-28T15:05:00")
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 4)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "3")
+    }
+    
+    func testLatestIatCertOnSameVaccinationDateButTwoOfThemHasNoIatButOneOfTheseTwoIsLatestVac() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        let vacinationDateLate = DateUtils.parseDate("2021-04-27T15:05:00")!
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-26T15:05:00")
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-27T15:05:00")
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-28T15:05:00")
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+        
+        var fifthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("5")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"5")
+        fifthCert.vaccinationCertificate.iat = nil
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            fifthCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 5)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "5")
+    }
+    
+    func testSortingVacBasedOnIatAndWithCertWhereIatIsNilAndOneCertificateIsNotPosNotPcr() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        let vacinationDateLate = DateUtils.parseDate("2021-04-27T15:05:00")!
+        let testDateLater = Date().addingTimeInterval(-86400)
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-26T15:05:00")
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-27T15:05:00")
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-28T15:05:00")
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+        
+        var fifthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("5")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"5")
+        fifthCert.vaccinationCertificate.iat = nil
+
+        
+        let sixtCert: ExtendedCBORWebToken = CBORWebToken
+            .mockTestCertificate
+            .mockTestUVCI("6")
+            .mockVaccinationSetDate(testDateLater)
+            .extended(vaccinationQRCodeData:"6")
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.tt = "NOT PCR"
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.tr = "NOT POSTIIVE"
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.sc = testDateLater
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            fifthCert,
+            sixtCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 6)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "6")
+    }
+    
+    
+    func testSortingVacBasedOnIatAndAllCerIatIsNil() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        let vacinationDateLate = DateUtils.parseDate("2021-04-27T15:05:00")!
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = nil
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = nil
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = nil
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+        
+        var fifthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("5")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"5")
+        fifthCert.vaccinationCertificate.iat = nil
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            fifthCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 5)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "5")
+    }
+    
+    
+    func testSortingVacBasedOnIatAndAllCerIatIsNilOneCertVacDateDiffer() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        let vacinationDateLate = DateUtils.parseDate("2021-04-27T15:05:00")!
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = nil
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = nil
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = nil
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+        
+        var fifthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("5")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"5")
+        fifthCert.vaccinationCertificate.iat = nil
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            fifthCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 5)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "2")
+    }
+    
+    func testSortingVacBasedOnIatAndWithCertWhereIatIsNilAndOneCertificateIsNotPosNotPcrWithIat() throws {
+        // GIVEN
+        let vacinationDate = DateUtils.parseDate("2021-04-26T15:05:00")!
+        let vacinationDateLate = DateUtils.parseDate("2021-04-27T15:05:00")!
+        let testDateYesterday = Date().addingTimeInterval(-86400)
+        var firstCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("1")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"1")
+        firstCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-26T15:05:00")
+
+        var secondCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("2")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"2")
+        secondCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-27T15:05:00")
+
+        var thirdCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("3")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"3")
+        thirdCert.vaccinationCertificate.iat = DateUtils.parseDate("2021-04-28T15:05:00")
+
+        var fourthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("4")
+            .mockVaccinationSetDate(vacinationDate)
+            .extended(vaccinationQRCodeData:"4")
+        fourthCert.vaccinationCertificate.iat = nil
+        
+        var fifthCert: ExtendedCBORWebToken = CBORWebToken
+            .mockVaccinationCertificate
+            .mockVaccinationUVCI("5")
+            .mockVaccinationSetDate(vacinationDateLate)
+            .extended(vaccinationQRCodeData:"5")
+        fifthCert.vaccinationCertificate.iat = nil
+        
+        var sixtCert: ExtendedCBORWebToken = CBORWebToken
+            .mockTestCertificate
+            .mockTestUVCI("6")
+            .extended(vaccinationQRCodeData:"6")
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.tt = "NOT PCR"
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.tr = "NOT POSTIIVE"
+        sixtCert.vaccinationCertificate.hcert.dgc.t!.first!.sc = testDateYesterday
+        sixtCert.vaccinationCertificate.iat = nil
+
+        var seventhCert: ExtendedCBORWebToken = CBORWebToken
+            .mockTestCertificate
+            .mockTestUVCI("7")
+            .extended(vaccinationQRCodeData:"7")
+        seventhCert.vaccinationCertificate.hcert.dgc.t!.first!.tt = "NOT PCR"
+        seventhCert.vaccinationCertificate.hcert.dgc.t!.first!.tr = "NOT POSTIIVE"
+        seventhCert.vaccinationCertificate.hcert.dgc.t!.first!.sc = testDateYesterday
+        seventhCert.vaccinationCertificate.iat = testDateYesterday
+
+        let certificates = [
+            secondCert,
+            firstCert,
+            fifthCert,
+            sixtCert,
+            seventhCert,
+            thirdCert,
+            fourthCert
+        ]
+        
+        // WHEN
+        let sortedCertifiates = CertificateSorter.sort(certificates)
+
+        // THEN
+        XCTAssertEqual(sortedCertifiates.count, 7)
+        XCTAssertEqual(sortedCertifiates[0].vaccinationCertificate.hcert.dgc.uvci, "7")
     }
 }
