8a23b14c36de19b372a92caa2f2772f1d6bde4a6
Kurt57 <karaaslan57@hotmail.de>
2021-11-18 11:48:01 +0100

    BVC-2949: add cryptoswift to target

--- Source/CovPassCommon/Package.swift
+++ Source/CovPassCommon/Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CovPassCommon",
-            dependencies: ["SwiftCBOR", "Scanner", "PromiseKit", "ASN1Decoder", "CertLogic", "JWTDecode"],
+            dependencies: ["SwiftCBOR", "Scanner", "PromiseKit", "ASN1Decoder", "CertLogic", "JWTDecode", "CryptoSwift"],
             resources: [.process("Resources")]
         ),
         .testTarget(
