91cdcd80d9575ec4980b8fb7c977770efc0cf2ec
Kurt57 <karaaslan57@hotmail.de>
2021-11-18 11:14:01 +0100

    BVC-2949: move lines to seperate files

--- Source/CovPassCommon/Sources/CovPassCommon/Repository/VAASRepository.swift
+++ Source/CovPassCommon/Sources/CovPassCommon/Repository/VAASRepository.swift
@@ -14,8 +14,8 @@ import SwiftCBOR
 import JWTDecode
 
 public protocol VAASRepositoryProtocol {
-    func decodeInitialisationQRCode(payload: String) -> ValidationServiceInitialisation?
     func fetchValidationService() -> Promise<AccessTokenResponse>
+    func validateTicketing (choosenCert cert: ExtendedCBORWebToken) throws -> Promise<Void>
 }
 
 public class VAASRepository: VAASRepositoryProtocol {
@@ -75,355 +75,103 @@ public class VAASRepository: VAASRepositoryProtocol {
         }
     }
     
-    public func decodeInitialisationQRCode(payload: String) -> ValidationServiceInitialisation? {
-        guard let data = payload.data(using: .utf8),
-              let ticket = try? JSONDecoder().decode(ValidationServiceInitialisation.self, from: data) else {
-                  return nil
-              }
-        self.ticket = ticket
-        return ticket
+    public func validateTicketing (choosenCert cert: ExtendedCBORWebToken) throws -> Promise<Void> {
+        guard let urlPath = accessTokenInfo?.aud!,
+              let url = URL(string: urlPath),
+              let iv = UserDefaults.standard.object(forKey: "xnonce"),
+              let verificationMethod = identityDocumentDecorator?.verificationMethod?.first(where: { $0.publicKeyJwk?.use == "enc" }) else  { throw APIError.invalidResponse }
+              let certificate = cert
+        
+        guard let dccData = encodeDCC(dgcString: certificate.vaccinationQRCodeData, iv: iv as! String),
+              let privateKey = Enclave.loadOrGenerateKey(with: "validationKey") else { throw APIError.invalidResponse }
+        
+        var sig = Data()
+        
+        Enclave.sign(data: dccData.0, with: privateKey, using: SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256, completion: { (signature,error) in
+            if let sign = signature {
+                sig = sign
+                let parameters = ["kid" : verificationMethod.publicKeyJwk!.kid, "dcc" : dccData.0.base64EncodedString(),
+                                  "sig": sig.base64EncodedString(),"encKey" : dccData.1.base64EncodedString(),
+                                  "sigAlg" : "SHA256withECDSA", "encScheme" : "RSAOAEPWithSHA256AESGCM"]
+                
+//                GatewayConnection.validateTicketing(url: url, parameters: parameters) { [weak self] responseModel in
+//                    DispatchQueue.main.async {
+//                        self?.stopActivity()
+//                        self?.performSegue(withIdentifier: Constants.showValidationResult, sender: responseModel)
+//                    }
+//                }
+            }
+        })
+        
+        return Promise.value
+    }
+    
+    private func encodeDCC(dgcString : String, iv: String) -> (Data,Data)? {
+        guard (iv.count > 16 || iv.count < 16 || iv.count % 8 > 0) else { return nil }
+        guard let verificationMethod = identityDocumentDecorator!.verificationMethod!.first(where: { $0.publicKeyJwk?.use == "enc" }) else { return nil }
+        
+        let ivData : [UInt8] = Array(base64: iv)
+        let dgcData : [UInt8] = Array(dgcString.utf8)
+        let _ : [UInt8] = Array(base64: verificationMethod.publicKeyJwk!.x5c.first!)
+        var encryptedDgcData : [UInt8] = Array()
+        
+        // AES GCM
+        let password: [UInt8] = Array("s33krit".utf8)
+        let salt: [UInt8] = Array("nacllcan".utf8)
+        
+        /* Generate a key from a `password`. Optional if you already have a key */
+        let key = try! PKCS5.PBKDF2(password: password, salt: salt, iterations: 4096, keyLength: 32, /* AES-256 */
+                                    variant: .sha2(.sha256)
+        ).calculate()
+        
+        let publicSecKey = VAASRepository.pubKey(from: verificationMethod.publicKeyJwk!.x5c.first!)
+        
+        do {
+            let gcm = GCM(iv: ivData, mode: .combined)
+            let aes = try AES(key: key, blockMode: gcm, padding: .noPadding)
+            encryptedDgcData = try aes.encrypt(dgcData)
+            //        let tag = gcm.authenticationTag
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        let encryptedKeyData = VAASRepository.encrypt(data: Data(key), with: publicSecKey!)
+        return (Data(encryptedDgcData), encryptedKeyData.0!)
+    }
+    
+    private static func encrypt(data: Data, with key: SecKey) -> (Data?, String?) {
+        guard let publicKey = SecKeyCopyPublicKey(key) else {
+            return (nil, ("err.pub-key-irretrievable"))
+        }
+        guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, SecKeyAlgorithm.rsaEncryptionOAEPSHA256) else {
+            return (nil, ("err.alg-not-supported"))
+        }
+        var error: Unmanaged<CFError>?
+        let cipherData = SecKeyCreateEncryptedData(publicKey, SecKeyAlgorithm.rsaEncryptionOAEPSHA256,
+                                                   data as CFData, &error) as Data?
+        let err = error?.takeRetainedValue().localizedDescription
+        return (cipherData, err)
+    }
+    
+    private func keyFromData(_ data: Data) throws -> SecKey {
+        let options: [String: Any] = [kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+                                      kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+                                      kSecAttrKeySizeInBits as String : 4096]
+        
+        var error: Unmanaged<CFError>?
+        guard let key = SecKeyCreateWithData(data as CFData,
+                                             options as CFDictionary, &error) else {
+            throw error!.takeRetainedValue() as Error
+        }
+        return key
+    }
+    
+    private static func pubKey(from b64EncodedCert: String) -> SecKey? {
+        guard
+            let encodedCertData = Data(base64Encoded: b64EncodedCert),
+            let cert = SecCertificateCreateWithData(nil, encodedCertData as CFData),
+            let publicKey = SecCertificateCopyKey(cert)
+        else { return nil }
+        return publicKey
     }
 }
-
-
-/*-
- * ---license-start
- * eu-digital-green-certificates / dgca-app-core-ios
- * ---
- * Copyright (C) 2021 T-Systems International GmbH and all other contributors
- * ---
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ---license-end
- */
-//
-//  X509.swift
-//
-//
-//  Created by Yannick Spreen on 4/17/21.
-//
-
-import Foundation
-import ASN1Decoder
-
-public class X509 {
-  
-  static let OID_TEST = "1.3.6.1.4.1.1847.2021.1.1"
-  static let OID_ALT_TEST = "1.3.6.1.4.1.0.1847.2021.1.1"
-  static let OID_VACCINATION = "1.3.6.1.4.1.1847.2021.1.2"
-  static let OID_ALT_VACCINATION = "1.3.6.1.4.1.0.1847.2021.1.2"
-  static let OID_RECOVERY = "1.3.6.1.4.1.1847.2021.1.3"
-  static let OID_ALT_RECOVERY = "1.3.6.1.4.1.0.1847.2021.1.3"
-  
-  public static func pubKey(from b64EncodedCert: String) -> SecKey? {
-    guard
-      let encodedCertData = Data(base64Encoded: b64EncodedCert),
-      let cert = SecCertificateCreateWithData(nil, encodedCertData as CFData),
-      let publicKey = SecCertificateCopyKey(cert)
-    else {
-      return nil
-    }
-    return publicKey
-  }
-  
-  public static func derPubKey(for secKey: SecKey) -> Data? {
-    guard
-      let pubKey = SecKeyCopyPublicKey(secKey)
-    else {
-      return nil
-    }
-    return derKey(for: pubKey)
-  }
-  
-  public static func derKey(for secKey: SecKey) -> Data? {
-    var error: Unmanaged<CFError>?
-    guard
-      let publicKeyData = SecKeyCopyExternalRepresentation(secKey, &error)
-    else {
-      return nil
-    }
-    return exportECPublicKeyToDER(publicKeyData as Data, keyType: kSecAttrKeyTypeEC as String, keySize: 384)
-  }
-  
-  static func exportECPublicKeyToDER(_ rawPublicKeyBytes: Data, keyType: String, keySize: Int) -> Data {
-    let curveOIDHeader: [UInt8] = [
-      0x30,
-      0x59,
-      0x30,
-      0x13,
-      0x06,
-      0x07,
-      0x2A,
-      0x86,
-      0x48,
-      0xCE,
-      0x3D,
-      0x02,
-      0x01,
-      0x06,
-      0x08,
-      0x2A,
-      0x86,
-      0x48,
-      0xCE,
-      0x3D,
-      0x03,
-      0x01,
-      0x07,
-      0x03,
-      0x42,
-      0x00
-    ]
-    var data = Data(bytes: curveOIDHeader, count: curveOIDHeader.count)
-    data.append(rawPublicKeyBytes)
-    return data
-  }
-  
-  static func checkisSuitable(cert: String, certType: CertType) -> Bool{
-    return isSuitable(cert: Data(base64Encoded:  cert)!, for: certType)
-  }
-  
-  static func isCertificateValid(cert: String) -> Bool {
-    guard let data = Data(base64Encoded:  cert) else { return true }
-    guard let certificate = try? X509Certificate(data: data) else {
-      return false
-    }
-    if (certificate.notAfter ?? Date()) > Date() {
-      return true
-    } else {
-      return false
-    }
-  }
-  
-  static func isSuitable(cert: Data,for certType: CertType) -> Bool {
-    guard let certificate = try? X509Certificate(data: cert) else {
-      return false
-    }
-    if isType(in: certificate) {
-      switch certType {
-      case .test:
-        return nil != certificate.extensionObject(oid: OID_TEST) || nil != certificate.extensionObject(oid: OID_ALT_TEST)
-      case .vaccination:
-        return nil != certificate.extensionObject(oid: OID_VACCINATION) || nil != certificate.extensionObject(oid: OID_ALT_VACCINATION)
-      case .recovery:
-        return nil != certificate.extensionObject(oid: OID_RECOVERY) || nil != certificate.extensionObject(oid: OID_ALT_RECOVERY)
-      }
-    }
-    return true
-  }
-  
-  static func isType(in certificate: X509Certificate) -> Bool {
-    return nil != certificate.extensionObject(oid: OID_TEST)
-      || nil != certificate.extensionObject(oid: OID_VACCINATION)
-      || nil != certificate.extensionObject(oid: OID_RECOVERY)
-      || nil != certificate.extensionObject(oid: OID_ALT_TEST)
-      || nil != certificate.extensionObject(oid: OID_ALT_VACCINATION)
-      || nil != certificate.extensionObject(oid: OID_ALT_RECOVERY)
-  }
-}
-
-
-//
-/*-
- * ---license-start
- * eu-digital-green-certificates / dgca-app-core-ios
- * ---
- * Copyright (C) 2021 T-Systems International GmbH and all other contributors
- * ---
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ---license-end
- */
-//
-//  Enclave.swift
-//
-//
-//  Created by Yannick Spreen on 4/25/21.
-//
-
-import Foundation
-
-public class Enclave {
-  static let encryptAlg = SecKeyAlgorithm.eciesEncryptionCofactorVariableIVX963SHA256AESGCM
-  static let signAlg = SecKeyAlgorithm.ecdsaSignatureMessageX962SHA512
-
-  static func tag(for name: String) -> Data {
-    "\(Bundle.main.bundleIdentifier ?? "app").\(name)".data(using: .utf8)!
-  }
-
-  static func generateKey(with name: String? = nil) -> (SecKey?, String?) {
-    let name = name ?? UUID().uuidString
-    let tag = Enclave.tag(for: name)
-    var error: Unmanaged<CFError>?
-    guard
-      let access =
-        SecAccessControlCreateWithFlags(
-          kCFAllocatorDefault,
-          kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
-          [.privateKeyUsage],
-          &error
-        )
-    else {
-      return (nil, error?.takeRetainedValue().localizedDescription)
-    }
-    var attributes: [String: Any] = [
-      kSecAttrKeyType as String: kSecAttrKeyTypeEC,
-      kSecAttrKeySizeInBits as String: 256,
-      kSecPrivateKeyAttrs as String: [
-        kSecAttrIsPermanent as String: true,
-        kSecAttrApplicationTag as String: tag,
-        kSecAttrAccessControl as String: access
-      ]
-    ]
-    #if !targetEnvironment(simulator)
-    attributes[kSecAttrTokenID as String] = kSecAttrTokenIDSecureEnclave
-    #endif
-    guard
-      let privateKey = SecKeyCreateRandomKey(
-        attributes as CFDictionary,
-        &error
-      )
-    else {
-      return (nil, error?.takeRetainedValue().localizedDescription)
-    }
-    return (privateKey, nil)
-  }
-
-  static func loadKey(with name: String) -> SecKey? {
-    let tag = Enclave.tag(for: name)
-    let query: [String: Any] = [
-      kSecClass as String: kSecClassKey,
-      kSecAttrKeyType as String: kSecAttrKeyTypeEC,
-      kSecAttrApplicationTag as String: tag,
-      kSecReturnRef as String: true
-    ]
-
-    var item: CFTypeRef?
-    let status = SecItemCopyMatching(query as CFDictionary, &item)
-    guard
-      status == errSecSuccess,
-      case let result as SecKey? = item
-    else {
-      return nil
-    }
-    return result
-  }
-
-  public static func loadOrGenerateKey(with name: String) -> SecKey? {
-    if let key = loadKey(with: name) {
-      return key
-    }
-    return generateKey(with: name).0
-  }
-
-  static func encrypt(data: Data, with key: SecKey) -> (Data?, String?) {
-    guard let publicKey = SecKeyCopyPublicKey(key) else {
-        return (nil, "err.pub-key-irretrievable")
-    }
-    guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, encryptAlg) else {
-      return (nil, "err.alg-not-supported")
-    }
-    var error: Unmanaged<CFError>?
-    let cipherData = SecKeyCreateEncryptedData(
-      publicKey,
-      encryptAlg,
-      data as CFData,
-      &error
-    ) as Data?
-    let err = error?.takeRetainedValue().localizedDescription
-    return (cipherData, err)
-  }
-
-  static func decrypt(data: Data, with key: SecKey, completion: @escaping (Data?, String?) -> Void) {
-    DispatchQueue.global(qos: .userInitiated).async {
-      let (result, error) = syncDecrypt(data: data, with: key)
-      completion(result, error)
-    }
-  }
-
-  static func syncDecrypt(data: Data, with key: SecKey) -> (Data?, String?) {
-    guard SecKeyIsAlgorithmSupported(key, .decrypt, encryptAlg) else {
-      return (nil, "err.alg-not-supported")
-    }
-    var error: Unmanaged<CFError>?
-    let clearData = SecKeyCreateDecryptedData(
-      key,
-      encryptAlg,
-      data as CFData,
-      &error
-    ) as Data?
-    let err = error?.takeRetainedValue().localizedDescription
-    return (clearData, err)
-  }
-
-  static func verify(data: Data, signature: Data, with key: SecKey) -> (Bool, String?) {
-    guard let publicKey = SecKeyCopyPublicKey(key) else {
-      return (false, "err.pub-key-irretrievable")
-    }
-    guard SecKeyIsAlgorithmSupported(publicKey, .verify, signAlg) else {
-      return (false, "err.alg-not-supported")
-    }
-    var error: Unmanaged<CFError>?
-    let isValid = SecKeyVerifySignature(
-      publicKey,
-      signAlg,
-      data as CFData,
-      signature as CFData,
-      &error
-    )
-    let err = error?.takeRetainedValue().localizedDescription
-    return (isValid, err)
-  }
-
-  public static func sign(
-    data: Data,
-    with key: SecKey,
-    using algorithm: SecKeyAlgorithm? = nil,
-    completion: @escaping (Data?, String?) -> Void
-  ) {
-    DispatchQueue.global(qos: .userInitiated).async {
-      let (result, error) = syncSign(data: data, with: key, using: algorithm)
-      completion(result, error)
-    }
-  }
-
-  static func syncSign(
-    data: Data,
-    with key: SecKey,
-    using algorithm: SecKeyAlgorithm? = nil
-  ) -> (Data?, String?) {
-    let algorithm = algorithm ?? signAlg
-    guard SecKeyIsAlgorithmSupported(key, .sign, algorithm) else {
-      return (nil, "err.alg-not-supported")
-    }
-    var error: Unmanaged<CFError>?
-    let signature = SecKeyCreateSignature(
-      key,
-      algorithm,
-      data as CFData,
-      &error
-    ) as Data?
-    let err = error?.takeRetainedValue().localizedDescription
-    return (signature, err)
-  }
-}
-
-
---
+++ Source/CovPassCommon/Sources/CovPassCommon/Repository/X509.swift
@@ -0,0 +1,146 @@
+/*-
+ * ---license-start
+ * eu-digital-green-certificates / dgca-app-core-ios
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+//
+//  X509.swift
+//
+//
+//  Created by Yannick Spreen on 4/17/21.
+//
+
+import Foundation
+import ASN1Decoder
+
+public class X509 {
+    
+    static let OID_TEST = "1.3.6.1.4.1.1847.2021.1.1"
+    static let OID_ALT_TEST = "1.3.6.1.4.1.0.1847.2021.1.1"
+    static let OID_VACCINATION = "1.3.6.1.4.1.1847.2021.1.2"
+    static let OID_ALT_VACCINATION = "1.3.6.1.4.1.0.1847.2021.1.2"
+    static let OID_RECOVERY = "1.3.6.1.4.1.1847.2021.1.3"
+    static let OID_ALT_RECOVERY = "1.3.6.1.4.1.0.1847.2021.1.3"
+    
+    public static func pubKey(from b64EncodedCert: String) -> SecKey? {
+        guard
+            let encodedCertData = Data(base64Encoded: b64EncodedCert),
+            let cert = SecCertificateCreateWithData(nil, encodedCertData as CFData),
+            let publicKey = SecCertificateCopyKey(cert)
+        else {
+            return nil
+        }
+        return publicKey
+    }
+    
+    public static func derPubKey(for secKey: SecKey) -> Data? {
+        guard
+            let pubKey = SecKeyCopyPublicKey(secKey)
+        else {
+            return nil
+        }
+        return derKey(for: pubKey)
+    }
+    
+    public static func derKey(for secKey: SecKey) -> Data? {
+        var error: Unmanaged<CFError>?
+        guard
+            let publicKeyData = SecKeyCopyExternalRepresentation(secKey, &error)
+        else {
+            return nil
+        }
+        return exportECPublicKeyToDER(publicKeyData as Data, keyType: kSecAttrKeyTypeEC as String, keySize: 384)
+    }
+    
+    static func exportECPublicKeyToDER(_ rawPublicKeyBytes: Data, keyType: String, keySize: Int) -> Data {
+        let curveOIDHeader: [UInt8] = [
+            0x30,
+            0x59,
+            0x30,
+            0x13,
+            0x06,
+            0x07,
+            0x2A,
+            0x86,
+            0x48,
+            0xCE,
+            0x3D,
+            0x02,
+            0x01,
+            0x06,
+            0x08,
+            0x2A,
+            0x86,
+            0x48,
+            0xCE,
+            0x3D,
+            0x03,
+            0x01,
+            0x07,
+            0x03,
+            0x42,
+            0x00
+        ]
+        var data = Data(bytes: curveOIDHeader, count: curveOIDHeader.count)
+        data.append(rawPublicKeyBytes)
+        return data
+    }
+    
+    static func checkisSuitable(cert: String, certType: CertType) -> Bool{
+        return isSuitable(cert: Data(base64Encoded:  cert)!, for: certType)
+    }
+    
+    static func isCertificateValid(cert: String) -> Bool {
+        guard let data = Data(base64Encoded:  cert) else { return true }
+        guard let certificate = try? X509Certificate(data: data) else {
+            return false
+        }
+        if (certificate.notAfter ?? Date()) > Date() {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    static func isSuitable(cert: Data,for certType: CertType) -> Bool {
+        guard let certificate = try? X509Certificate(data: cert) else {
+            return false
+        }
+        if isType(in: certificate) {
+            switch certType {
+            case .test:
+                return nil != certificate.extensionObject(oid: OID_TEST) || nil != certificate.extensionObject(oid: OID_ALT_TEST)
+            case .vaccination:
+                return nil != certificate.extensionObject(oid: OID_VACCINATION) || nil != certificate.extensionObject(oid: OID_ALT_VACCINATION)
+            case .recovery:
+                return nil != certificate.extensionObject(oid: OID_RECOVERY) || nil != certificate.extensionObject(oid: OID_ALT_RECOVERY)
+            }
+        }
+        return true
+    }
+    
+    static func isType(in certificate: X509Certificate) -> Bool {
+        return nil != certificate.extensionObject(oid: OID_TEST)
+        || nil != certificate.extensionObject(oid: OID_VACCINATION)
+        || nil != certificate.extensionObject(oid: OID_RECOVERY)
+        || nil != certificate.extensionObject(oid: OID_ALT_TEST)
+        || nil != certificate.extensionObject(oid: OID_ALT_VACCINATION)
+        || nil != certificate.extensionObject(oid: OID_ALT_RECOVERY)
+    }
+}
+
+
