8852440dc41400a9f4157304ebdb1c2fbf963c6d
Kurt57 <karaaslan57@hotmail.de>
2021-11-18 11:29:19 +0100

    BVC-2949: fix tests

--- Source/CovPassCommon/Tests/CovPassCommonTests/API/APIServiceMock.swift
+++ Source/CovPassCommon/Tests/CovPassCommonTests/API/APIServiceMock.swift
@@ -12,6 +12,17 @@ import PromiseKit
 @testable import CovPassCommon
 
 class APIServiceMock: APIServiceProtocol {
+    
+    var vaasListOfServicesResponse: Promise<String>?
+    func vaasListOfServices(url: URL) -> Promise<String> {
+        vaasListOfServicesResponse ?? Promise.value("")
+    }
+    
+    var accesTokenResponse: Promise<String>?
+    func getAccessTokenFor(url: URL, servicePath: String, publicKey: String) -> Promise<String> {
+        accesTokenResponse ?? Promise.value("")
+    }
+    
     var trustListResult: Promise<String>?
     func fetchTrustList() -> Promise<String> {
         trustListResult ?? Promise.value("")
