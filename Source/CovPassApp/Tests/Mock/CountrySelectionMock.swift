2d4b1490b3fbb74ca4f821e2f593b53de5da4de1
Fatih Karakurt-external <hallo@fatihkarakurt.de>
2022-02-03 12:48:05 +0100

    Twine Update, Add tests for country selection, test order (#212)
    Co-authored-by: Kurt57 <karaaslan57@hotmail.de>

---
+++ Source/CovPassApp/Tests/Mock/CountrySelectionMock.swift
@@ -0,0 +1,55 @@
+//
+//  CountrySelectionMock.swift
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import CovPassCommon
+
+struct CountrySelectionMock {
+    static var countries = [Country("IT"),
+                            Country("DE"),
+                            Country("DE2"),
+                            Country("LT"),
+                            Country("DK"),
+                            Country("GR"),
+                            Country("CZ"),
+                            Country("HR"),
+                            Country("IS"),
+                            Country("PT"),
+                            Country("PL"),
+                            Country("BE"),
+                            Country("BG"),
+                            Country("LU"),
+                            Country("EE"),
+                            Country("CY"),
+                            Country("ES"),
+                            Country("NL"),
+                            Country("AT"),
+                            Country("LV"),
+                            Country("LI"),
+                            Country("FI"),
+                            Country("SE"),
+                            Country("SI"),
+                            Country("RO"),
+                            Country("NO"),
+                            Country("SK"),
+                            Country("FR"),
+                            Country("MT"),
+                            Country("HU"),
+                            Country("IE"),
+                            Country("CH"),
+                            Country("VA"),
+                            Country("SM"),
+                            Country("UA"),
+                            Country("TR"),
+                            Country("MK"),
+                            Country("AD"),
+                            Country("MC"),
+                            Country("FO"),
+                            Country("MA"),
+                            Country("AL"),
+                            Country("IL"),
+                            Country("PA")]
+}
---
+++ Source/CovPassApp/Tests/Mock/CountrySelectionRouterMock.swift
@@ -0,0 +1,12 @@
+//
+//  CountrySelectionRouterMock.swift
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+@testable import CovPassApp
+
+class CountrySelectionRouterMock: CountrySelectionRouterProtocol {
+    
+}
