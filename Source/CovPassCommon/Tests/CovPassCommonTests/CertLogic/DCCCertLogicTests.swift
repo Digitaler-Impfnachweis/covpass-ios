ff5957e16ef02acb40b560fe8f3d0cb85c80b5fd
Fatih Karakurt-external <hallo@fatihkarakurt.de>
2022-03-30 11:00:44 +0200

    no message (#321)
    Co-authored-by: Kurt57 <karaaslan57@hotmail.de>

--- Source/CovPassCommon/Tests/CovPassCommonTests/CertLogic/DCCCertLogicTests.swift
+++ Source/CovPassCommon/Tests/CovPassCommonTests/CertLogic/DCCCertLogicTests.swift
@@ -64,7 +64,7 @@ class DCCCertLogicTests: XCTestCase {
         XCTAssertEqual(sut.valueSets.count, 8)
         XCTAssertEqual(sut.valueSets["country-2-codes"]?.count, 250)
         XCTAssertEqual(sut.valueSets["covid-19-lab-result"]?.count, 2)
-        XCTAssertEqual(sut.valueSets["covid-19-lab-test-manufacturer-and-name"]?.count, 207)
+        XCTAssertEqual(sut.valueSets["covid-19-lab-test-manufacturer-and-name"]?.count, 214)
         XCTAssertEqual(sut.valueSets["covid-19-lab-test-type"]?.count, 2)
         XCTAssertEqual(sut.valueSets["disease-agent-targeted"]?.count, 1)
         XCTAssertEqual(sut.valueSets["sct-vaccines-covid-19"]?.count, 3)
