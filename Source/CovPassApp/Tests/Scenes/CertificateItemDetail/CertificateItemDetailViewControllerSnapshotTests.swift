e70ce5b561b7d4a9bc2947baf176906e9850996c
Thomas Kulessa-external <business@tkulessa.de>
2022-06-29 09:44:47 +0200

    BVC-4468: Change text. Add snapshot test. (#440)

--- Source/CovPassApp/Tests/Scenes/CertificateItemDetail/CertificateItemDetailViewControllerSnapshotTests.swift
+++ Source/CovPassApp/Tests/Scenes/CertificateItemDetail/CertificateItemDetailViewControllerSnapshotTests.swift
@@ -47,4 +47,29 @@ class CertificateItemDetailViewControllerSnapshotTests: BaseSnapShotTests {
         // When & Then
         verifyView(view: viewController.view, height: 1800)
     }
+
+    func testExpiringCertificate() throws {
+        // Given
+        var token = try ExtendedCBORWebToken.token1Of1()
+        token.vaccinationCertificate.exp = Date(timeIntervalSinceNow: 60)
+        let viewModel = CertificateItemDetailViewModelMock(
+            router: CertificateItemDetailRouterMock(),
+            repository: VaccinationRepositoryMock(),
+            certificate: token,
+            resolvable: nil,
+            vaasResultToken: nil
+        )
+        let sut = CertificateItemDetailViewController(
+            viewModel: viewModel
+        )
+
+        // When & Then
+        verifyView(view: sut.view)
+    }
+}
+
+private class CertificateItemDetailViewModelMock: CertificateItemDetailViewModel {
+    override var expiresSoonDate: Date? {
+        Date(timeIntervalSinceReferenceDate: 0)
+    }
 }
