386955b16df1a436c6a8ddb06302f276fae8dfbb
Thomas Kuleßa <business@tkulessa.de>
2022-09-23 12:31:11 +0200

    Add protocols.

---
+++ Source/CovPassCheckApp/Source/Scenes/Mask Required Result/Protocols/MaskRequiredResultViewModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MaskRequiredResultViewModelProtocol.swift
+//
+//  © Copyright IBM Deutschland GmbH 2021
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+protocol MaskRequiredResultViewModelProtocol {
+    var title: String { get }
+    var subtitle: String { get }
+    var description: String { get }
+    var reasonViewModels: [MaskRequiredReasonViewModelProtocol] { get }
+}
