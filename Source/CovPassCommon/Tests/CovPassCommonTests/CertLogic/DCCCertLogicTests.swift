d692b76df7574c9ba49e713eb961a57a8e73f5bd
Fatih Karakurt-external <hallo@fatihkarakurt.de>
2023-01-06 11:00:19 +0100

    update (#739)
    Co-authored-by: Kurt57 <karaaslan57@hotmail.de>

--- Source/CovPassCommon/Tests/CovPassCommonTests/CertLogic/DCCCertLogicTests.swift
+++ Source/CovPassCommon/Tests/CovPassCommonTests/CertLogic/DCCCertLogicTests.swift
@@ -66,7 +66,7 @@ class DCCCertLogicTests: XCTestCase {
         XCTAssertEqual(sut.valueSets.count, 8)
         XCTAssertEqual(sut.valueSets["country-2-codes"]?.count, 250)
         XCTAssertEqual(sut.valueSets["covid-19-lab-result"]?.count, 2)
-        XCTAssertEqual(sut.valueSets["covid-19-lab-test-manufacturer-and-name"]?.count, 266)
+        XCTAssertEqual(sut.valueSets["covid-19-lab-test-manufacturer-and-name"]?.count, 276)
         XCTAssertEqual(sut.valueSets["covid-19-lab-test-type"]?.count, 2)
         XCTAssertEqual(sut.valueSets["disease-agent-targeted"]?.count, 1)
         XCTAssertEqual(sut.valueSets["sct-vaccines-covid-19"]?.count, 6)
@@ -139,7 +139,7 @@ class DCCCertLogicTests: XCTestCase {
 
     func testSavedAndLocalRules() throws {
         // Check local rules (no saved rules)
-        XCTAssertEqual(sut.dccRules.count, 284)
+        XCTAssertEqual(sut.dccRules.count, 286)
 
         // Save one rule
         let rule = Rule(identifier: "", type: "", version: "", schemaVersion: "", engine: "", engineVersion: "", certificateType: "", description: [], validFrom: "", validTo: "", affectedString: [], logic: JSON(""), countryCode: "")
